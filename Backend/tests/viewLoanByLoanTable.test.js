e4c3a313e4e4eb95f1a13bc632c46829ca038009
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-09-12 22:35:28 +0530

    update the test case according to the request

--- Backend/tests/viewLoanByLoanTable.test.js
+++ Backend/tests/viewLoanByLoanTable.test.js
@@ -13,9 +13,9 @@ import viewLoanByLoanId from "../controller.js/view-loanController.js";
 
 // setup express app for testing
 const app = express();
-app.get("/loan/:loan_id", viewLoanByLoanId);
+app.get("/view-loan/:loan_id", viewLoanByLoanId);
 
-describe("GET /loan/:loan_id", () => {
+describe("GET /view-loan/:loan_id", () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
@@ -23,32 +23,32 @@ describe("GET /loan/:loan_id", () => {
   it("should return loan details with customer info", async () => {
     // mock DB return
     LoanExcel.findOne.mockResolvedValue({
-      loan_id: 1313,
-      loan_amount: 50000,
-      interest_rate: 12,
-      monthly_payment: 4500,
-      tenure: 12,
-      CustomerExcel: {
-        customer_id: 10,
-        first_name: "Bhauhvan",
-        last_name: "Chauhan",
-        phone_number: "9037913092",
-        age: 21
-      }
-    });
+    "loan_id": 7997,
+    "customer": {
+        "customer_id": 244,
+        "first_name": "Annabel",
+        "last_name": "Soria",
+        "phone_number": 9490132408,
+        "age": 48
+    },
+    "loan_amount": 400000,
+    "interest_rate": 12.1,
+    "monthly_installment": 10408,
+    "tenure": 135
+});
 
-    const res = await request(app).get("/loan/1313");
+    const res = await request(app).get("/view-loan/1313");
 
     expect(res.status).toBe(200);
     expect(res.body.loan_id).toBe(1);
-    expect(res.body.customer.first_name).toBe("Bhauhvan");
+    expect(res.body.customer.first_name).toBe("Annabel");
     expect(res.body.loan_amount).toBe(50000);
   });
 
   it("should return 404 if loan not found", async () => {
     LoanExcel.findOne.mockResolvedValue(null);
 
-    const res = await request(app).get("/loan/999");
+    const res = await request(app).get("/view-loan/999");
 
     expect(res.status).toBe(404);
     expect(res.body.message).toBe("Loan not found");
